23db54e8205c03b9910b0600ac41589b39957227
Peter Schreyer <prhschreyer@gmail.com>
2024-10-29 14:27:45 -0700

    Build out foundation for server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import { Button } from '@nextui-org/react';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
+import Profile from '@/components/Profile';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click me!</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+
+      <Profile />
     </div>
   );
 }
