24791f3960ef6a45c07651fbdc7ccd2b0bc66b50
Peter Schreyer <prhschreyer@gmail.com>
2024-10-30 14:42:49 -0700

    Form validation

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,50 @@
 'use server';
 
-export async function createTopic() {
+import { z } from 'zod';
+import { auth } from '@/auth';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData,
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  const session = await auth();
+  if (!session?.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to create a post'],
+      },
+    };
+  }
+
+  return {
+    errors: {},
+  };
   // TODO: revalidate homepage
 }
---
+++ src/components/topics/TopicCreateForm.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@nextui-org/react';
+import * as actions from '@/actions';
+import { useActionState } from 'react';
+
+export default function TopicCreateForm() {
+  // formData type needs to match server action type
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {},
+  });
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={action}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Topic</h3>
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
+            />
+            {formState.errors._form && (
+              <div className="p-2 rounded bg-red-200 border border-red-400">
+                {formState.errors._form?.join(', ')}
+              </div>
+            )}
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
